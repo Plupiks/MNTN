6a170c974df03d3a2ee97829ed72c83fe701e2ca
Vitaliy Vasylykiv <92222356+Plupiks@users.noreply.github.com>
2021-11-14 21:08:51 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const logoIcon = $('.account-body .account-icon')
 const logoText = $('.account-title')
+const logoTextMob = $('.account-title--mobile')
 const body = $('.account-body')
+const bodyMob = $('.account-body-mobile')
 const navButton = $('.burger-menu')
 const mobileMenu = $('.mobile-menu')
 const mediaQuery = window.matchMedia('(max-width: 800px)')
@@ -24,18 +26,23 @@ function menuOpen() {
 
 
 
-if (mediaQueryMin.matches) {
-  body.on('mouseenter', function(){
-    $(logoIcon).animate({ "left": "8px", "opacity": "1" }, "600" );
-    $(logoText).animate({ "left": "8px" }, "600" ).css({"color": "#fbd784"});
-  });
+body.on('mouseenter', function(){
+  $(logoIcon).animate({ "left": "8px", "opacity": "1" }, "600" );
+  $(logoText).animate({ "left": "8px" }, "600" ).css({"color": "#fbd784"});
+});
 
-  body.on('mouseleave', function(){
-    $(logoIcon).animate({ "left": "24px", "opacity": "0" }, "600" )
-    $(logoText).animate({ "left": "0" }, "600" ).css({"color": "#fff"});
-  });
-}
+body.on('mouseleave', function(){
+  $(logoIcon).animate({ "left": "24px", "opacity": "0" }, "600" )
+  $(logoText).animate({ "left": "0" }, "600" ).css({"color": "#fff"});
+});
 
+bodyMob.on('mouseenter', function(){
+  logoTextMob.css({'color': '#fbd784'})
+});
+
+bodyMob.on('mouseleave', function(){
+  logoTextMob.css({'color': '#fff'})
+});
 
 $(window).click(function(){
   if (mobileMenu.hasClass('mobile-menu-active')) {
@@ -53,9 +60,15 @@ function menuRemoveClass() {
   }
 }
 
-$(window).on('resize load',function(){
+$(window).on('resize',function(){
   menuRemoveClass();
 })
+
+$(window).on('resize', 'load', function(){
+  menuOpen();
+})
+
+
 
 
 mobileMenu.click(function(e){
